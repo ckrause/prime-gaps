d09f2dec9dfa4623caf928b4c1d3be013dca2897
Christian Krause <me@ckrause.org>
2019-01-10 21:33:57 +0100

    some funny patterns

--- gaps.cpp
+++ gaps.cpp
@@ -1,11 +1,16 @@
 #include <deque>
 #include <iostream>
+#include <sstream>
 
 void prime_gaps( int n )
 {
   int prime = 1; // will store current prime number
   int gap = 1; // stores the gap to the next prime number
   std::deque<int> next_gaps = { 1 }; // list of next gap
+
+  std::deque<char> survivors = { '-' };
+  char s;
+  size_t sl = 300;
 
   for ( int i = 1; i <= n; i++ )
   {
@@ -15,22 +20,33 @@ void prime_gaps( int n )
     next_gaps.pop_front(); // move next gap from front...
     next_gaps.push_back( gap ); // ...to end
 
+    s = survivors.front();
+    survivors.pop_front();
+    survivors.push_back( s );
+
     std::deque<int> updated_gaps;
+    std::deque<char> updated_survivors;
 
     // make prime number copies of the list
     for ( int j = 0; j < prime; j++ )
     {
       std::copy( next_gaps.begin(), next_gaps.end(), std::back_inserter( updated_gaps ) );
+      std::copy( survivors.begin(), survivors.end(), std::back_inserter( updated_survivors ) );
 
       // since the original algorithm has factorial space complexity
       // we use the following check to stop generating more gaps when
-      // we get close to the target size; not part of orginal algorithm!
-      if ( updated_gaps.size() > 2 * n )
+      // we get close to the target size; not part of original algorithm!
+      if ( updated_gaps.size() > 10 * n )
       {
         break;
       }
 
     }
+
+    // TODO: update survivors here
+    updated_survivors.pop_back();
+
+    std::string df;
 
     // remove illegal gaps from the list
     int sum = gap;
@@ -42,16 +58,40 @@ void prime_gaps( int n )
         sum += updated_gaps[j + 1];
         updated_gaps[j] += updated_gaps[j + 1];
         updated_gaps.erase( updated_gaps.begin() + j + 1 );
+        if ( df.size() < sl ) df.push_back('X');
       }
+      else if ( df.size() < sl ) df.push_back('-');
+
     }
     next_gaps = updated_gaps;
 
+    survivors = updated_survivors;
+
+
+    // print debug info
+    std::string ss;
+    for (char d : survivors)
+    {
+  	  ss.push_back(d);
+  	  if (ss.size() == sl) break;
+    }
+//    std::cout << "Step " << i << ": prime=" << prime << "; gap=" << gap << ", survivors=" << ss.str() << std::endl;
     std::cout << "Step " << i << ": prime=" << prime << "; gap=" << gap << std::endl;
+    std::cout << "expected: " << df << std::endl;
+    std::cout << "     got: " << ss << std::endl << std::endl;
+
+    // checks
+    if ( survivors.size() != next_gaps.size() )
+    {
+//    	throw std::runtime_error("incorrect length of survivors: expected " + std::to_string( next_gaps.size() ) + ", got " + std::to_string( survivors.size() ) );
+
+    }
+
 
   }
 }
 
 int main( int argc, char *argv[] )
 {
-  prime_gaps( 10000 );
+  prime_gaps( 20 );
 }
