53d16cbbf8f14c20e0878b47bb1c93078e6bc212
Christian Krause <D060111@c02v23lahv2q.fritz.box>
2019-10-19 18:53:04 +0200

    compare with groundtruth

--- gaps.cpp
+++ gaps.cpp
@@ -4,79 +4,89 @@
 #include <iostream>
 #include <sstream>
 
-void prime_gaps( int n )
+struct state
 {
-  int prime = 1; // will store current prime number
-  int gap = 1; // stores the gap to the next prime number
-  std::deque<int> next_gaps = { 1 }; // list of next gap
+  int prime; // will store current prime number
+  int gap; // stores the gap to the next prime number
+  std::deque<int> next_gaps; // list of next gap
+};
 
-  for ( int i = 1; i <= n; i++ )
+void next( state &s, int max )
+{
+  s.prime += s.gap; // next prime is current prime plus gap
+
+  s.gap = s.next_gaps.front(); // next gap is first gap from list
+  s.next_gaps.pop_front(); // move next gap from front...
+  s.next_gaps.push_back( s.gap ); // ...to end
+
+  std::deque<int> updated_gaps;
+
+  // make prime number copies of the list
+  for ( int j = 0; j < s.prime; j++ )
   {
-    prime += gap; // next prime is current prime plus gap
-    gap = next_gaps.front(); // next gap is first gap from list
+    std::copy( s.next_gaps.begin(), s.next_gaps.end(), std::back_inserter( updated_gaps ) );
 
-    next_gaps.pop_front(); // move next gap from front...
-    next_gaps.push_back( gap ); // ...to end
-
-    std::deque<int> updated_gaps;
-
-    // make prime number copies of the list
-    for ( int j = 0; j < prime; j++ )
+    // since the original algorithm has factorial space complexity
+    // we use the following check to stop generating more gaps when
+    // we get close to the target size; not part of original algorithm!
+    if ( updated_gaps.size() > 2 * max )
     {
-      std::copy( next_gaps.begin(), next_gaps.end(), std::back_inserter( updated_gaps ) );
-
-      // since the original algorithm has factorial space complexity
-      // we use the following check to stop generating more gaps when
-      // we get close to the target size; not part of original algorithm!
-      if ( updated_gaps.size() > 2 * n )
-      {
-        break;
-      }
-
+      break;
     }
-
-    // remove illegal gaps from the list
-    int sum = gap;
-    for ( int j = 0; j < (int) updated_gaps.size(); j++ )
-    {
-      sum += updated_gaps[j];
-      if ( sum % prime == 0 )
-      {
-        sum += updated_gaps[j + 1];
-        updated_gaps[j] += updated_gaps[j + 1];
-        updated_gaps.erase( updated_gaps.begin() + j + 1 );
-      }
-    }
-    next_gaps = updated_gaps;
-
-    std::cout << "Step " << i << ": prime=" << prime << "; gap=" << gap << std::endl;
 
   }
+
+  // remove illegal gaps from the list
+  int sum = s.gap;
+  for ( int j = 0; j < (int) updated_gaps.size(); j++ )
+  {
+    sum += updated_gaps[j];
+    if ( sum % s.prime == 0 )
+    {
+      sum += updated_gaps[j + 1];
+      updated_gaps[j] += updated_gaps[j + 1];
+      updated_gaps.erase( updated_gaps.begin() + j + 1 );
+    }
+  }
+  s.next_gaps = updated_gaps;
 }
 
-std::vector<int> load_ground_truth() {
-	std::ifstream bfile("b001223.txt");
-	if (!bfile.good())
-		throw std::runtime_error("b-file not found");
-	std::string l;
-	std::vector<int> seq;
-	int64_t expected_index = 1, index = 0, value = 0;
-	while (std::getline(bfile, l)) {
-		if (l.empty() || l[0] == '#')
-			continue;
-		std::stringstream ss(l);
-		ss >> index >> value;
-		if (index != expected_index || value < 1)
-			throw std::runtime_error("error in b-file");
-		seq.push_back(value);
-		++expected_index;
-	}
-	return seq;
+std::vector<int> load_ground_truth()
+{
+  std::ifstream bfile( "b001223.txt" );
+  if ( !bfile.good() ) throw std::runtime_error( "b-file not found" );
+  std::string l;
+  std::vector<int> seq;
+  int64_t expected_index = 1, index = 0, value = 0;
+  while ( std::getline( bfile, l ) )
+  {
+    if ( l.empty() || l[0] == '#' ) continue;
+    std::stringstream ss( l );
+    ss >> index >> value;
+    if ( index != expected_index || value < 1 ) throw std::runtime_error( "error in b-file" );
+    seq.push_back( value );
+    ++expected_index;
+  }
+  return seq;
 }
 
 int main( int argc, char *argv[] )
 {
-	auto data = load_ground_truth();
-	std::cout << "found: " << data.at(10) << std::endl;
-//  prime_gaps( 10000 );
+  auto ground_truth = load_ground_truth();
+
+  state s;
+  s.prime = 1;
+  s.gap = 1;
+  s.next_gaps = { 1 };
+
+  int max = 10000;
+  for ( int i = 0; i < max; i++ )
+  {
+    next( s, max );
+    std::cout << "Step " << i << ": prime=" << s.prime << "; gap=" << s.gap << std::endl;
+    if ( s.gap != ground_truth.at( i ) )
+    {
+      throw std::runtime_error( "unexpected gap!" );
+    }
+  }
 }
